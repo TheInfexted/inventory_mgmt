66d9897c5bf9b899d2d591090aba9c63985e7c38
brendany <79629480+TheInfexted@users.noreply.github.com>
2024-12-27 16:38:10 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = 'mysql://avnadmin:AVNS_FeJq0NNjborXlDVHDN6@mysql-invmgmt1127-inventory-mgmt.g.aivencloud.com:12193/defaultdb?ssl-mode=REQUIRED'
+    SQLALCHEMY_DATABASE_URI = 'mysql://avnadmin:AVNS_FeJq0NNjborXlDVHDN6@mysql-invmgmt1127-inventory-mgmt.g.aivencloud.com:12193/defaultdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

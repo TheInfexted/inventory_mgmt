80abf29aaad16e307b820f3e42e00597ad7fda3d
brendany <79629480+TheInfexted@users.noreply.github.com>
2024-12-27 14:40:14 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://my_user:userpassword@%:3306/my_database'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://my_user:userpassword@localhost:3306/my_database'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
